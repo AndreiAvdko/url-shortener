c0055f6bf12fa53af2bdd63fdcb3d0bf2b62b14b
Andrei Avdeenko <andrei_avdeenko@outlook.com>
2024-12-30 01:15:08 +0300

    create service

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-default:"local" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" end_default:"localhost:8080"`
+	Timeout     time.Duration `yaml"timeout" env_default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env_default:"60s"`
+}
+
+// Must - если вместо возврата ошибки - функция бросает панику
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check if file exist
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
